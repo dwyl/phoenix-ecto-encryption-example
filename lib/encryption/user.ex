683e3fc462782466fc9ee206d12b333617e91d94
nelsonic <contact.nelsonic+github@gmail.com>
2018-01-24 22:48:06 +0000

    update definition of lib/encryption/user.ex (User Schema) to use EncryptedField as per https://github.com/danielberkompas/phoenix_ecto_encryption_sample/blob/80c9b75a39f89a203f80617e2c00c062e9904217/web/models/user.ex

--- lib/encryption/user.ex
+++ lib/encryption/user.ex
@@ -5,17 +5,35 @@ defmodule Encryption.User do
 
 
   schema "users" do
-    field :email, :binary
-    field :email_hash, :binary
-    field :name, :binary
+    field :email, Encryption.EncryptedField
+    field :email_hash, Encryption.HashField
+    field :name, Encryption.EncryptedField
 
     timestamps()
   end
 
-  @doc false
+  # Ensure that hashed fields never get out of date
+  # before_insert :set_hashed_fields
+  # before_update :set_hashed_fields
+
+  @required_fields ~w(name email)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the user and attrs
+  """
   def changeset(%User{} = user, attrs) do
+    IO.inspect user
     user
     |> cast(attrs, [:name, :email, :email_hash])
     |> validate_required([:name, :email, :email_hash])
+    |> set_hashed_fields
+    |> validate_unique(:email_hash, on: Encryption.Repo)
+  end
+
+  defp set_hashed_fields(changeset) do
+    IO.inspect changeset
+    changeset
+    |> put_change(:email_hash, changeset.changes[:email] || changeset.model.email)
   end
 end
