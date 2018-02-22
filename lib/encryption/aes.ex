043ec3b27ca588eb8125325ad4129c4a2ff046ec
nelsonic <contact.nelsonic+github@gmail.com>
2018-02-22 23:19:27 +0000

    default to :aes_gcm for encryption

--- lib/encryption/aes.ex
+++ lib/encryption/aes.ex
@@ -6,61 +6,6 @@ defmodule Encryption.AES do
   """
   @aad "AES256GCM"
 
-  @doc """
-  Encrypt a value. Uses a random IV for each call, and prepends the IV to the
-  ciphertext.  This means that `encrypt/1` will never return the same ciphertext
-  for the same value. This makes "cracking" (bruteforce decryption) much harder!
-  ## Parameters
-  - `plaintext`: Accepts any data type as all values are converted to a String
-    using `to_string` before encryption.
-  ## Examples
-      iex> Encryption.AES.encrypt("test") != Encryption.AES.encrypt("test")
-      true
-      iex> ciphertext = Encryption.AES.encrypt(123)
-      ...> is_binary(ciphertext)
-      true
-  """
-  @spec encrypt(any) :: String.t
-  def encrypt(plaintext) do
-    iv    = :crypto.strong_rand_bytes(16) # create random Initialisation Vector
-    state = :crypto.stream_init(:aes_ctr, get_key(), iv) # create crypto stream
-    # peform the encryption:
-    {_state, ciphertext} = :crypto.stream_encrypt(state, to_string(plaintext))
-    iv <> ciphertext # "return" iv concatenated with the ciphertext
-  end
-
-  @doc """
-  Decrypt a binary.
-
-  ## Parameters
-  - `ciphertext`: a binary to decrypt, assuming that the first 16 bytes of the
-    binary are the IV to use for decryption.
-  - `key_id`: the index of the AES encryption key used to encrypt the ciphertext
-  ## Example
-      iex> Encryption.AES.encrypt("test") |> Encryption.AES.decrypt(1)
-      "test"
-  """
-  @spec decrypt(String.t, number) :: {String.t, number}
-  def decrypt(ciphertext, key_id) do
-    <<iv::binary-16, ciphertext::binary>> = ciphertext # split iv & ciphertext
-    # get encryption key based on key_id & Initialise crypto stream:
-    state = :crypto.stream_init(:aes_ctr, get_key(key_id), iv)
-    # perform decryption
-    {_state, plaintext} = :crypto.stream_decrypt(state, ciphertext)
-    plaintext # "return" just the plaintext
-  end
-
-  # as above but *asumes* `default` (latest) encryption key is used.
-  def decrypt(ciphertext) do
-    <<iv::binary-16, ciphertext::binary>> = ciphertext # split iv & ciphertext
-    # get encryption key based on key_id & Initialise crypto stream:
-    state = :crypto.stream_init(:aes_ctr, get_key(), iv)
-    # perform decryption
-    {_state, plaintext} = :crypto.stream_decrypt(state, ciphertext)
-    plaintext # "return" just the plaintext
-  end
-
-
   defp get_key do
     keys = Application.get_env(:encryption, Encryption.AES)[:keys]
     count = Enum.count(keys) - 1
@@ -92,34 +37,35 @@ defmodule Encryption.AES do
   ## Parameters
   - `plaintext`: Accepts any data type as all values are converted to a String
     using `to_string` before encryption.
+  - `key_id`: the index of the AES encryption key used to encrypt the ciphertext
   ## Examples
-      iex> Encryption.AES.encrypt("test") != Encryption.AES.encrypt("test")
+      iex> Encryption.AES.encrypt("tea", 1) != Encryption.AES.encrypt("tea", 1)
       true
-      iex> ciphertext = Encryption.AES.encrypt(123)
-      ...> is_binary(ciphertext)
+      iex> ciphertext = Encryption.AES.encrypt(123, 1)
+      iex> is_binary(ciphertext)
       true
   """
-  @spec encrypt_gcm(any, number) :: {String.t, number}
-  def encrypt_gcm(plaintext, key_id) do
+  @spec encrypt(any, number) :: {String.t, number}
+  def encrypt(plaintext, key_id) do
     IO.inspect plaintext, label: "plaintext"
     iv = :crypto.strong_rand_bytes(16) # create random Initialisation Vector
     IO.inspect iv, label: "iv"
     key = get_key(key_id)
     {ciphertext, tag} =
-      :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, plaintext, 16})
+      :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, to_string(plaintext), 16})
 
     IO.inspect tag, label: "tag"
     # {:ok, Encoder.encode(tag) <> iv <> ciphertag <> ciphertext}
     iv <> tag <> ciphertext # "return" iv with the cipher tag & ciphertext
   end
 
-  def encrypt_gcm(plaintext) do
+  def encrypt(plaintext) do
     IO.inspect plaintext, label: "plaintext"
     iv = :crypto.strong_rand_bytes(16) # create random Initialisation Vector
     IO.inspect iv, label: "iv"
     key = get_key()
     {ciphertext, tag} =
-      :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, plaintext, 16})
+      :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, to_string(plaintext), 16})
 
     IO.inspect tag, label: "tag"
     # {:ok, Encoder.encode(tag) <> iv <> ciphertag <> ciphertext}
@@ -137,8 +83,8 @@ defmodule Encryption.AES do
       iex> Encryption.AES.encrypt("test") |> Encryption.AES.decrypt(1)
       "test"
   """
-  @spec decrypt_gcm(String.t, number) :: {String.t, number}
-  def decrypt_gcm(ciphertext, key_id) do
+  @spec decrypt(String.t, number) :: {String.t, number}
+  def decrypt(ciphertext, key_id) do
     <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
 
     IO.inspect key_id, label: "key_id"
@@ -155,7 +101,7 @@ defmodule Encryption.AES do
   end
 
   # as above but *asumes* `default` (latest) encryption key is used.
-  def decrypt_gcm(ciphertext) do
+  def decrypt(ciphertext) do
     <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
     :crypto.block_decrypt(:aes_gcm, get_key(), iv, {@aad, ciphertext, tag})
   end
--- test/lib/aes_test.exs
+++ test/lib/aes_test.exs
@@ -8,11 +8,16 @@ defmodule Encryption.AESTest do
     assert AES.encrypt("hello") != "hello"
   end
 
+  test ".encrypt can encrypt a number" do
+    assert is_binary(AES.encrypt(123, 1))
+  end
+
   test ".encrypt includes the random IV in the value" do
     <<iv::binary-16, ciphertext::binary>> = AES.encrypt("hello")
 
     assert String.length(iv) != 0
     assert String.length(ciphertext) != 0
+    assert is_binary(ciphertext)
   end
 
   test ".encrypt does not produce the same ciphertext twice" do
@@ -30,16 +35,16 @@ defmodule Encryption.AESTest do
     keys = Application.get_env(:encryption, Encryption.AES)[:keys]
     key_id = Enum.count(keys) - 1
     IO.inspect key_id, label: "key_id"
-    ciphertext = AES.encrypt_gcm("hello", key_id)
+    ciphertext = AES.encrypt("hello", key_id)
     IO.inspect ciphertext, label: "ciphertext"
     # plaintext = "hello" |> AES.encrypt_gcm |> AES.decrypt_gcm(key_id)
-    plaintext = AES.decrypt_gcm(ciphertext, key_id)
+    plaintext = AES.decrypt(ciphertext, key_id)
     IO.inspect plaintext, label: "plaintext"
     assert plaintext == "hello"
   end
 
   test "can decrypt_gcm with default key" do
-    plaintext = "hello" |> AES.encrypt_gcm |> AES.decrypt_gcm()
+    plaintext = "hello" |> AES.encrypt |> AES.decrypt()
     assert plaintext == "hello"
   end
 end
