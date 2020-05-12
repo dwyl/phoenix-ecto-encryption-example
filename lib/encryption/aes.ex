51ba83aca358f02b07241e92c45c08f893e36aec
Frank Oxener <frank.oxener@gmail.com>
2020-05-12 21:35:14 +0200

    Store key_id as part of th encrypted binary (suggestion of @moritzploss in issue #33) in order to make full use of the Ecto-Type behaviour and prevent unnecessary double load and dump and to make key rotation really work

--- lib/encryption/aes.ex
+++ lib/encryption/aes.ex
@@ -6,7 +6,8 @@ defmodule Encryption.AES do
   this makes "bruteforce" decryption much more difficult.
   See `encrypt/1` and `decrypt/1` for more details.
   """
-  @aad "AES256GCM" # Use AES 256 Bit Keys for Encryption.
+  # Use AES 256 Bit Keys for Encryption.
+  @aad "AES256GCM"
 
   @doc """
   Encrypt Using AES Galois/Counter Mode (GCM)
@@ -17,30 +18,23 @@ defmodule Encryption.AES do
   ## Parameters
   - `plaintext`: Accepts any data type as all values are converted to a String
     using `to_string` before encryption.
-  - `key_id`: the index of the AES encryption key used to encrypt the ciphertext
   ## Examples
-      iex> Encryption.AES.encrypt("tea", 1) != Encryption.AES.encrypt("tea", 1)
+      iex> Encryption.AES.encrypt("tea") != Encryption.AES.encrypt("tea")
       true
-      iex> ciphertext = Encryption.AES.encrypt(123, 1)
+      iex> ciphertext = Encryption.AES.encrypt(123)
       iex> is_binary(ciphertext)
       true
   """
-  @spec encrypt(any) :: String.t
+  @spec encrypt(any) :: String.t()
   def encrypt(plaintext) do
-    iv = :crypto.strong_rand_bytes(16) # create random Initialisation Vector
-    key = get_key()    # get the *latest* key in the list of encryption keys
-    {ciphertext, tag} =
-      :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, to_string(plaintext), 16})
-    iv <> tag <> ciphertext # "return" iv with the cipher tag & ciphertext
-  end
-
-  @spec encrypt(any, number) :: {String.t, number}
-  def encrypt(plaintext, key_id) do
-    iv = :crypto.strong_rand_bytes(16) # create random Initialisation Vector
-    key = get_key(key_id) # get *specific* key (by id) from list of keys.
-    {ciphertext, tag} =
-      :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, to_string(plaintext), 16})
-    iv <> tag <> ciphertext # "return" iv with the cipher tag & ciphertext
+    iv = :crypto.strong_rand_bytes(16)
+    # get latest key
+    key = get_key()
+    # get latest ID;
+    key_id = get_key_id()
+    # {ciphertext, tag} = :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, plaintext, 16})
+    {ciphertext, tag} = :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, to_string(plaintext), 16})
+    iv <> tag <> <<key_id::unsigned-big-integer-32>> <> ciphertext
   end
 
   @doc """
@@ -50,21 +44,15 @@ defmodule Encryption.AES do
     binary are the IV to use for decryption.
   - `key_id`: the index of the AES encryption key used to encrypt the ciphertext
   ## Example
-      iex> Encryption.AES.encrypt("test") |> Encryption.AES.decrypt(1)
+      iex> Encryption.AES.encrypt("test") |> Encryption.AES.decrypt()
       "test"
   """
-  @spec decrypt(String.t, number) :: {String.t, number}
-  def decrypt(ciphertext, key_id) do # patern match on binary to split parts:
-    <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
-    key = get_key(key_id) # get encrytion/decryption key based on key_id
-    :crypto.block_decrypt(:aes_gcm, key, iv, {@aad, ciphertext, tag})
-  end
-
-  # as above but *asumes* `default` (latest) encryption key is used.
-  @spec decrypt(any) :: String.t
+  @spec decrypt(any) :: String.t()
   def decrypt(ciphertext) do
-    <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
-    :crypto.block_decrypt(:aes_gcm, get_key(), iv, {@aad, ciphertext, tag})
+    <<iv::binary-16, tag::binary-16, key_id::unsigned-big-integer-32, ciphertext::binary>> =
+      ciphertext
+
+    :crypto.block_decrypt(:aes_gcm, get_key(key_id), iv, {@aad, ciphertext, tag})
   end
 
   # @doc """
@@ -77,16 +65,23 @@ defmodule Encryption.AES do
   #     iex> Encryption.AES.get_key
   #     <<13, 217, 61, 143, 87, 215, 35, 162, 183, 151, 179, 205, 37, 148>>
   # """ # doc commented out because https://stackoverflow.com/q/45171024/1148249
-  @spec get_key() :: String
+  @spec get_key() :: String.t()
   defp get_key do
-    keys = Application.get_env(:encryption, Encryption.AES)[:keys]
-    count = Enum.count(keys) - 1
-    get_key(count)
+    get_key_id() |> get_key
   end
 
-  @spec get_key(number) :: String
+  @spec get_key(number) :: String.t()
   defp get_key(key_id) do
-    keys = Application.get_env(:encryption, Encryption.AES)[:keys]
-    Enum.at(keys, key_id)
+    encryption_keys() |> Enum.at(key_id)
+  end
+
+  @spec get_key_id() :: integer()
+  defp get_key_id do
+    Enum.count(encryption_keys()) - 1
+  end
+
+  @spec encryption_keys() :: list(binary())
+  defp encryption_keys do
+    Application.get_env(:encryption, Encryption.AES)[:keys]
   end
 end
--- test/lib/aes_test.exs
+++ test/lib/aes_test.exs
@@ -9,7 +9,7 @@ defmodule Encryption.AESTest do
   end
 
   test ".encrypt can encrypt a number" do
-    assert is_binary(AES.encrypt(123, 1))
+    assert is_binary(AES.encrypt(123))
   end
 
   test ".encrypt includes the random IV in the value" do
@@ -20,27 +20,35 @@ defmodule Encryption.AESTest do
     assert is_binary(ciphertext)
   end
 
+  test ".encrypt includes the key_id in the value" do
+    <<_iv::binary-16, _tag::binary-16, key_id::unsigned-big-integer-32, _ciphertext::binary>> =
+      AES.encrypt("hello")
+
+    assert key_id == 0
+  end
+
   test ".encrypt does not produce the same ciphertext twice" do
     assert AES.encrypt("hello") != AES.encrypt("hello")
   end
 
   test "can decrypt a value" do
-    keys = Application.get_env(:encryption, Encryption.AES)[:keys]
-    key_id = Enum.count(keys) - 1
-    plaintext = "hello" |> AES.encrypt |> AES.decrypt(key_id)
+    plaintext = "hello" |> AES.encrypt() |> AES.decrypt()
     assert plaintext == "hello"
   end
 
-  test "can decrypt/2 a value" do
-    keys = Application.get_env(:encryption, Encryption.AES)[:keys]
-    key_id = Enum.count(keys) - 1
-    ciphertext = AES.encrypt("hello", key_id)
-    plaintext = AES.decrypt(ciphertext, key_id)
-    assert plaintext == "hello"
-  end
+  test "can still decrypt the value after adding a new encryption key" do
+    encrypted_value = "hello" |> AES.encrypt()
 
-  test "decrypt/1 ciphertext that was encrypted with default key" do
-    plaintext = "hello" |> AES.encrypt |> AES.decrypt()
-    assert plaintext == "hello"
+    original_keys = Application.get_env(:encryption, Encryption.AES)[:keys]
+
+    # add a new key
+    Application.put_env(:encryption, Encryption.AES,
+      keys: original_keys ++ [:crypto.strong_rand_bytes(32)]
+    )
+
+    assert "hello" == encrypted_value |> AES.decrypt()
+
+    # rollback to the original keys
+    Application.put_env(:encryption, Encryption.AES, keys: original_keys)
   end
 end
